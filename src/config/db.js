c34b59d5928b41f4a0ebcb4cc87b3020ab71d474
Nilesh7Mehta <98638808+Nilesh7Mehta@users.noreply.github.com>
2026-06-03 16:23:26 +0530

    added supabase connection

--- src/config/db.js
+++ src/config/db.js
@@ -7,13 +7,20 @@ dotenv.config();
 
 const { Pool } = pg;
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   ssl: false, // set true in production if needed
+// });
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: false, // set true in production if needed
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // for development, set to true in production
+  },
 });
 
 
